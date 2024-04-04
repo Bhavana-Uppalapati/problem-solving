a2a1a2e3111bc9f59110bc1de14f8b7743be7066
Bhavana-Uppalapati <ubhavana1012@gmail.com>
2024-04-04 16:27:14 +0530

    pattrens

--- pattrens/JS/3-4-2024 pattrens.js
+++ pattrens/JS/3-4-2024 pattrens.js
@@ -97,3 +97,63 @@ for(let i=1;i<=n;i++){
     console.log(str)
 }
 
+//6.
+// *
+// **
+// * *
+// *  *
+// *   *
+// ******
+// let n=Number(prompt("Enter number"))
+// for(i=1;i<=n;i++){
+//     let str=" "
+//     for(j=1;j<=n;j++){
+//         if(i==n || j==1){
+//             str+= "*"
+//         }else if(j===i){
+//             str+="*"
+//         }
+//         else{
+//             str+= " "
+//         }
+//     }console.log(str)
+// }
+
+//7.
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// let h=Number(prompt("enter the height"))
+// for(let i=1;i<=h;i++){
+//     let str=''
+//     for(let j=1;j<=h-i;j++){
+//         str+=' '
+//     }
+//     for(let k=1;k<=(2*i)-1;k++){
+//         str+='*'
+//     }console.log(str)
+// }
+
+//8.
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+
+// let h=Number(prompt("enter the height"))
+
+// for(let i=0;i<=h;i++){
+//     let str=""
+//     for(let j=0;j<=i-1;j++){
+//       str+=' '
+//     }
+//     for(let k=1;k<=2*(h-i)-1;k++){
+//         str+="*"
+//     }console.log(str)
+// }
+
